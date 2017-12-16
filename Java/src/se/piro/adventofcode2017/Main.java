d04f6792b11fe7ab5c035e625490a2f624e943eb
Rolf Staflin <rex@piro.se>
2017-12-16 15:31:07 +0100

    Day 16: Reimplementation in Java. Down to 1 ms per iteration, but that's still too long.

---
+++ Java/src/se/piro/adventofcode2017/Main.java
@@ -0,0 +1,10 @@
+package se.piro.adventofcode2017;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Day16 day = new Day16();
+        day.solve();
+    }
+}
